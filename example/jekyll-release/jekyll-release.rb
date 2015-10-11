d369a069fed0baadcf2a5776dde8728585da58c8
Nishant Rodrigues <nishantjr@gmail.com>
2015-10-11 23:12:03 +0800

    Add 'jekyll-release' gem to the 'example/' directory.
    This gem provides a 'release' command for jekyll that commits the generated site
    to the 'gh-pages' branch of the git repository. This makes it possible to commit
    the build without using multiple checkouts or switching branches and moving
    files around.
    
    At the moment the target branch is not configurable.

---
+++ example/jekyll-release/jekyll-release.rb
@@ -0,0 +1,22 @@
+class ReleaseCommand < Jekyll::Command
+    class << self
+        def init_with_program(prog)
+            prog.command(:release) { |c|
+                c.syntax "release"
+                c.description 'Release current build to gh-pages branch.'
+                c.action { |args, options|
+                    build_options = options.dup
+                    build_options['quiet'] = true
+                    Jekyll::Commands::Build.process(build_options)
+                    process(options)
+                }
+            }
+        end
+
+        def process(options)
+            dest = configuration_from_options(options)['destination']
+            system(Gem.bin_path('jekyll-release', 'git-commit-filetree'), 'gh-pages', dest) \
+                || fail('git-commit-filetree failed.')
+        end
+    end
+end
